4c825c78461151a9d77942023a4f2869de7d6b5d
mola <lamstouray@gmail.com>
2023-03-16 03:01:46 +0000

    update

--- Code02.sh
+++ Code02.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Script: Ops301d6 Challenge-01
+# Script: Ops301d6 Challenge-02
 # Author: Lamin Touray
-# Purpose:
+# Purpose:Create a new bash script that performs the following:
+# Prompts user for input directory path.
+# Prompts user for input permissions number (e.g. 777 to perform a chmod 777)
+# Navigates to the directory input by the user and changes all files inside it to the input setting.
+# Prints to the screen the directory contents and the new permissions settings of everything in the directory.
 
 
 # Prompts user for input directory path.
