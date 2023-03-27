0c8f9838bb19cacf670e5ecce45863c669b58bcb
mola <lamstouray@gmail.com>
2023-03-27 06:15:40 +0000

    changes

--- Code10.py
+++ Code10.py
@@ -3,24 +3,30 @@
 # Author: Lamin Touray
 # Purpose: Using file handling commands, create a Python script that creates a new .txt file, appends three lines, prints to the screen the first line, then deletes the .txt file
 
+# newfile = open("test.txt", "w")
 
+# # How to open a file
+# openfile = open("test.txt")
+
+# # How to return the five first characters of a file
+# read = open("test.txt", "r")
+# print("test.txt"(readline(1))
 
 
 
 
 # Open a new file in write mode
-with open('example.txt', 'w') as file:
-    # Write three lines to the file
-    file.write('This is the first line.\n')
-    file.write('This is the second line.\n')
-    file.write('This is the third line.\n')
+with open("example.txt", "w") as file:
+    file.write("This is the first line.\n")
+    file.write("This is the second line.\n")
+    file.write("This is the third line.\n")
 
 # Open the file in read mode
-with open('example.txt', 'r') as file:
-    # Read the first line of the file and print it to the screen
-    first_line = file.readline()
+with open("example.txt", "r") as file:
+    for i in range(3):
+     first_line = file.readline()
     print(first_line)
 
 # Delete the file
 import os
-os.remove('example.txt')
+os.remove("example.txt")
